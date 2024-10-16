6731b3d2a39af82045e2d34f67a182f152083fd1
Germán <germancardozo1802@gmail.com>
2024-10-16 19:31:41 -0300

    implementamos iniciar sesion

--- Ejercicio1/bash.sh
+++ Ejercicio1/bash.sh
@@ -6,13 +6,13 @@ menuRegistro() {
     while [ $volver -eq 0 ]
     do
         echo "Ingrese su nombre de Usuario (x) para volver"
-        read -p "-> " usuario 
+        read -p "-> " usuario
         
         if [ "$usuario" = "x" ] || [ "$usuario" = "X" ]
         then
             volver=1
         else
-            verificacion= VerificarUsuario $usuario
+            verificacion= VerificarRegistroUser $usuario
             verificacion=$?
             if [ $verificacion -eq 1 ]
             then
@@ -36,12 +36,12 @@ menuRegistro() {
     done
 }
 
-VerificarUsuario() {
+VerificarRegistroUser() {
     clear
     valido=0
     
     #$1 usuario
-    if [[ $1 != "" ]] && [[ $1 != *" "* ]] && [[ $1 != *":"* ]] && [[ $1 != *"Anuar"* ]] #es una joda q>:P
+    if [[ $1 != "" ]] && [[ $1 != *" "* ]] && [[ $1 != *":"* ]]
      then
         if ! grep -Fq "$1:" "./Users.txt" && ! grep -Fq "$1:" "./Admins.txt"
         then
@@ -56,34 +56,6 @@ VerificarUsuario() {
     fi
     
     return $valido
-}
-
-menuPrincipal() {
-    clear
-    salir=0
-
-    while [ $salir -eq 0 ]
-    do
-        echo "Para iniciar sesion opción 1."
-        echo "Para registrarse como usuario opción 2."
-        echo "Para registrarse como Admin opción 3."
-        echo "Para salir opcion 0."
-        read -p "-> " opcion
-
-        if [[ $opcion -eq 0 ]]
-        then
-            salir=1
-        elif [[ $opcion -eq 2 || $opcion -eq 3 ]]
-        then
-            menuRegistro $opcion $salir
-        else
-            clear
-            echo "Opción no válida. Intente nuevamente."
-            sleep 2
-        fi
-
-    done
-    
 }
 
 registrar() {
@@ -101,6 +73,106 @@ registrar() {
     fi
     echo "$1:$2" >> "$archivo"
     echo "" >> "$archivo"
+    
+}
+
+menuIniSesion() {
+    clear
+    volver=0
+
+    while [ $volver -eq 0 ]
+    do
+        clear
+        echo "Ingrese su nombre de Usuario (x) para volver"
+        read -p "-> " usuario
+
+        if [ "$usuario" = "x" ] || [ "$usuario" = "X" ]
+        then
+            volver=1
+        else
+            echo "Ingrese su contraseña (x) para volver"
+            read -p "-> " contrasenia
+
+            if [ ! "$contrasenia" = "x" ] && [ ! "$contrasenia" = "X" ]
+            then
+                verificar= verificarLoggeo $usuario $contrasenia
+                verificar=$? 
+                if [ $verificar -eq 1 ]
+                then
+                    menuAdmin
+                elif [ $verificar -eq 2 ]
+                then    
+                    menuUser
+                fi
+                volver=1
+            fi
+        fi
+    done
+
+}
+
+menuAdmin() {
+    echo soy Admin
+    sleep 2
+}
+
+menuUser() {
+    echo soy el user, chele
+    sleep 2
+}
+
+verificarLoggeo() {
+    clear
+    valido=0
+    
+    #$1 usuario
+    #$2 contrasenia
+    
+    if grep -Fq "$1:$2" "./Admins.txt" 
+    then
+        valido=1  
+    else
+        if grep -Fq "$1:$2" "./Users.txt" 
+        then
+            valido=2
+        else
+            echo "Usuario no registrado"
+            sleep 1
+        fi
+    fi
+    
+    return $valido
+}
+
+menuPrincipal() {
+    clear
+    salir=0
+
+    while [ $salir -eq 0 ]
+    do
+        echo "Para iniciar sesion opción 1."
+        echo "Para registrarse como usuario opción 2."
+        echo "Para registrarse como Admin opción 3."
+        echo "Para salir opcion 0."
+        read -p "-> " opcion
+
+        if [ $opcion -eq 0 ]
+        then
+            salir=1
+        elif [ $opcion -eq 1 ]
+        then
+            menuIniSesion
+            clear
+        elif [[ $opcion -eq 2 || $opcion -eq 3 ]]
+        then
+            menuRegistro $opcion $salir
+        else
+            clear
+            echo "Opción no válida. Intente nuevamente."
+            sleep 2
+        fi
+
+    done
     
 }
 
