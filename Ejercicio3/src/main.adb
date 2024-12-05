91406151e1911e33f3155abf84d62114241fb78c
MatiProtocol <matibalas@gmail.com>
2024-12-05 19:38:17 -0300

    el gepeto me hizo la cola en ada.

--- Ejercicio3/src/main.adb
+++ Ejercicio3/src/main.adb
@@ -1,149 +1,154 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure LMC_Simulation is
+procedure Main is
 
-   -- Constantes para definir la memoria y sem醘oros
-   Memory_Size : constant := 128;
-   Num_Semaphores : constant := 16;
+   -- Tama帽o de la memoria y cantidad de sem谩foros
+   Memoria_Size : constant Integer := 128;
+   Semaforo_Count : constant Integer := 16;
 
-   -- Tipo expl韈ito para la memoria compartida
-   type Memory_Type is array (0 .. Memory_Size - 1) of Integer;
+   -- Definici贸n del arreglo de memoria compartida
+   type Memory_Array is array (0 .. Memoria_Size - 1) of Integer;
+   Shared_Memory : Memory_Array := (others => 0);
 
-   -- Tipo protegido para la memoria compartida
-   protected type Shared_Memory is
-      procedure Write(Address : Integer; Value : Integer);
-      function Read(Address : Integer) return Integer;
-   private
-      Mem : Memory_Type := (others => 0); -- Inicializaci髇 expl韈ita
-   end Shared_Memory;
+   -- Inicializaci贸n de la variable compartida en memoria
+   Valor_Pos : constant Integer := 100; -- Posici贸n de la variable `valor`
 
-   protected body Shared_Memory is
-      procedure Write(Address : Integer; Value : Integer) is
-      begin
-         Mem(Address) := Value;
-      end Write;
 
-      function Read(Address : Integer) return Integer is
-      begin
-         return Mem(Address);
-      end Read;
-   end Shared_Memory;
+   -- Definici贸n del tipo de tarea para los sem谩foros
+   task type Semaforo is
+      entry Init(Value : Integer);
+      entry Wait;
+      entry Signal;
+   end Semaforo;
 
-   -- Memoria compartida
-   Memory : Shared_Memory;
-
-   -- Tipo protegido para sem醘oros
-   protected type Semaphore is
-      procedure Init(Value : Integer);
-      procedure Wait;
-      procedure Signal;
-   private
-      Counter : Integer := 0;
-   end Semaphore;
-
-   protected body Semaphore is
-      procedure Init(Value : Integer) is
-      begin
-         Counter := Value;
-      end Init;
-
-      procedure Wait is
-      begin
-         while Counter = 0 loop
-            delay 0.01; -- Simula espera activa (puedes usar entrada/salida si se prefiere)
-         end loop;
-         Counter := Counter - 1;
-      end Wait;
-
-      procedure Signal is
-      begin
-         Counter := Counter + 1;
-      end Signal;
-   end Semaphore;
-
-   -- Array de sem醘oros
-   type Semaphore_Array is array (0 .. Num_Semaphores - 1) of Semaphore;
-   Semaphores : Semaphore_Array;
-
-   -- Tipo de registro de la CPU
-   type CPU_Registers is record
-      Acc : Integer := 0; -- Acumulador
-      IP  : Integer := 0; -- Puntero de instrucci髇
-      ID  : Integer := 0; -- Identificador 鷑ico de CPU
-   end record;
-
-   -- Tareas de CPU
-   task type CPU_Task(ID : Integer) is
-      entry Start;
-   end CPU_Task;
-
-   task body CPU_Task is
-      Regs : CPU_Registers := (Acc => 0, IP => 0, ID => ID);
+   task body Semaforo is
+      S : Integer := 0;
    begin
-      accept Start;
       loop
-         declare
-            Instruction : Integer := Memory.Read(Regs.IP);
-         begin
-            case Instruction is
-               when 100 => -- LOAD
-                  Regs.Acc := Memory.Read(Memory.Read(Regs.IP + 1));
-                  Regs.IP := Regs.IP + 2;
-               when 200 => -- STORE
-                  Memory.Write(Memory.Read(Regs.IP + 1), Regs.Acc);
-                  Regs.IP := Regs.IP + 2;
-               when 300 => -- ADD
-                  Regs.Acc := Regs.Acc + Memory.Read(Memory.Read(Regs.IP + 1));
-                  Regs.IP := Regs.IP + 2;
-               when 400 => -- SUB
-                  Regs.Acc := Regs.Acc - Memory.Read(Memory.Read(Regs.IP + 1));
-                  Regs.IP := Regs.IP + 2;
-               when 500 => -- BRCPU
-                  if Regs.ID = Memory.Read(Regs.IP + 1) then
-                     Regs.IP := Memory.Read(Regs.IP + 2);
-                  else
-                     Regs.IP := Regs.IP + 3;
-                  end if;
-               when 600 => -- SEMINIT
-                  Semaphores(Memory.Read(Regs.IP + 1)).Init(Memory.Read(Regs.IP + 2));
-                  Regs.IP := Regs.IP + 3;
-               when 700 => -- SEMWAIT
-                  Semaphores(Memory.Read(Regs.IP + 1)).Wait;
-                  Regs.IP := Regs.IP + 2;
-               when 800 => -- SEMSIGNAL
-                  Semaphores(Memory.Read(Regs.IP + 1)).Signal;
-                  Regs.IP := Regs.IP + 2;
-               when 900 => -- HALT
-                  exit;
-               when others =>
-                  Put_Line("Unknown Instruction");
-                  exit;
-            end case;
-         end;
+         select
+            when S > 0 =>
+               accept Wait do
+                  S := S - 1;
+               end Wait;
+         or
+            accept Signal do
+               S := S + 1;
+            end Signal;
+         or
+            accept Init(Value : Integer) do
+               S := Value;
+            end Init;
+         end select;
       end loop;
-   end CPU_Task;
+   end Semaforo;
 
-   -- Instancias de CPU
-   CPU0 : CPU_Task(0);
-   CPU1 : CPU_Task(1);
+   -- Declaraci贸n de sem谩foros
+   Semaforo_Mutex : Semaforo; -- Para controlar la exclusi贸n mutua
+   Semaforo_Sync : Semaforo;  -- Para sincronizar las CPUs
+
+   -- Definici贸n del tipo de tarea para la memoria compartida
+   task Memoria is
+      entry Leer(Pos : in Integer; Val : out Integer);
+      entry Escribir(Pos : in Integer; Val : in Integer);
+   end Memoria;
+
+   task body Memoria is
+   begin
+      loop
+         select
+            accept Leer(Pos : in Integer; Val : out Integer) do
+               Val := Shared_Memory(Pos);
+            end Leer;
+         or
+            accept Escribir(Pos : in Integer; Val : in Integer) do
+               Shared_Memory(Pos) := Val;
+            end Escribir;
+         end select;
+      end loop;
+   end Memoria;
+
+   -- Definici贸n del tipo de tarea para los CPUs
+   task type CPU(Number : Integer) is
+      entry Start;
+   end CPU;
+
+   task body CPU is
+      A : Integer := 0;  -- Acumulador
+      IP : Integer := 0; -- Instruction Pointer
+      Instruction, Operand1, Operand2 : Integer;
+   begin
+      accept Start do
+         loop
+            -- Leer la instrucci贸n desde la memoria
+            Memoria.Leer(IP, Instruction);
+            Memoria.Leer(IP + 1, Operand1);
+            if Instruction in 6 .. 8 then
+               Memoria.Leer(IP + 2, Operand2);
+            end if;
+
+            -- Ejecutar la instrucci贸n
+            case Instruction is
+               when 1 =>  -- LOAD
+                  Memoria.Leer(Operand1, A);
+               when 2 =>  -- STORE
+                  Memoria.Escribir(Operand1, A);
+               when 3 =>  -- ADD
+                  Memoria.Leer(Operand1, Operand2);
+                  A := A + Operand2;
+               when 4 =>  -- SUB
+                  Memoria.Leer(Operand1, Operand2);
+                  A := A - Operand2;
+               when 5 =>  -- BRCPU
+                  if Number = Operand1 then
+                     IP := Operand2;
+                  end if;
+               when 6 =>  -- SEMINIT
+                  Semaforo_Mutex.Init(Operand2);
+               when 7 =>  -- SEMWAIT
+                  Semaforo_Mutex.Wait;
+               when 8 =>  -- SEMSIGNAL
+                  Semaforo_Mutex.Signal;
+               when others =>
+                  exit; -- Termina cuando alcanza una instrucci贸n no v谩lida
+            end case;
+
+            IP := IP + 2;
+         end loop;
+      end Start;
+   end CPU;
+
+   -- Declaraci贸n de dos CPUs concurrentes
+   CPU_1 : CPU(0);
+   CPU_2 : CPU(1);
 
 begin
-   -- Inicializaci髇 de memoria y sem醘oros
-   Memory.Write(0, 100); -- LOAD 0
-   Memory.Write(1, 0);
-   Memory.Write(2, 300); -- ADD 1
-   Memory.Write(3, 1);
-   Memory.Write(4, 200); -- STORE 2
-   Memory.Write(5, 2);
-   Memory.Write(6, 900); -- HALT
+   -- Inicializaci贸n de sem谩foros
+   Semaforo_Mutex.Init(1);  -- Inicializaci贸n del sem谩foro Mutex
+   Semaforo_Sync.Init(0);   -- Inicializaci贸n del sem谩foro Sync
 
-   -- Sem醘oros inicializados en 0
-   for I in Semaphores'Range loop
-      Semaphores(I).Init(0);
-   end loop;
+   -- Asignaci贸n inicial de la variable en la memoria
+   Shared_Memory(Valor_Pos) := 8;
 
-   -- Iniciar CPUs
-   CPU0.Start;
-   CPU1.Start;
+   -- Cargar Instrucciones Iniciales
+   Shared_Memory(0) := 1; -- LOAD valor
+   Shared_Memory(1) := Valor_Pos;
+   Shared_Memory(2) := 2; -- STORE
+   Shared_Memory(3) := Valor_Pos;
 
-end LMC_Simulation;
+   -- Iniciar las CPUs
+   CPU_1.Start;
+   CPU_2.Start;
+
+   -- Esperar a que ambas CPUs terminen
+   Semaforo_Sync.Wait;
+   Semaforo_Sync.Wait;
+
+   -- Mostrar el resultado final
+   declare
+      Resultado : Integer;
+   begin
+      Memoria.Leer(Valor_Pos, Resultado);
+      Put_Line("Resultado Final: " & Integer'Image(Resultado));
+   end;
+end Main;
